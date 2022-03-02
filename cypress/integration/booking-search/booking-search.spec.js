4604a048b8ea24db368325e8e88ff658534914a8
nenadt85 <tripunovicnenad@gmail.com>
2022-03-02 15:05:26 +0100

    additional corrections

--- cypress/integration/booking-search/booking-search.spec.js
+++ cypress/integration/booking-search/booking-search.spec.js
@@ -9,16 +9,25 @@ describe('Booking.com Hotel Search', () => {
       bookingSearch = seacrhDetails;
       });
 
+    })
+
+    beforeEach(()=> {
+
       cy.visit('https://www.booking.com/')
     })
   
+    it('should search a booking without enetered detination', () => {
+      cy.get('.sb-searchbox__outer button[type=submit]').click();
+      cy.get('.sb-searchbox__outer .fe_banner__message').should('contain', 'Enter a destination to start searching.');
+    })
+
     it('should serach for specific hotel', () => {
       cy.get('.sb-searchbox__outer form input[type=search]').type(bookingSearch.cityName);
      
       //I used {force: true} because this element is covered by another element, but we can solve this using eq() instead contains(), too
       cy.get('.sb-searchbox__outer .sb-date-field.b-datepicker').contains('Check-in').click({force:true});
-      cy.get('.sb-searchbox__outer .bui-calendar__date span[aria-label="26 February 2022"]').click();
       cy.get('.sb-searchbox__outer .bui-calendar__date span[aria-label="2 March 2022"]').click();
+      cy.get('.sb-searchbox__outer .bui-calendar__date span[aria-label="5 March 2022"]').click();
 
       cy.get('.sb-searchbox__outer .xp__guests__count').click();
       cy.get('.sb-searchbox__outer button[aria-label="Increase number of Children"]').click();
@@ -26,10 +35,20 @@ describe('Booking.com Hotel Search', () => {
 
       cy.get('.sb-searchbox__outer button[type=submit]').click();
 
-      cy.get('#basiclayout li button[aria-label=" 3"]').click();
-      
-      cy.get('#basiclayout h3 .fde444d7ef').should('contain', 'Agate Hôtel');
+      cy.get('#basiclayout h3 .fde444d7ef')
+      .invoke('text')
+      .then(($hotelName)=> {
+        let i =1;
+        while(i<=40) {
+            if(!$hotelName.includes(bookingSearch.hotelName)) {
+              cy.get('#basiclayout li button').contains(i).click();
+              i++;
+            }
+            else {
+              cy.get('#basiclayout h3 .fde444d7ef').should('contain', bookingSearch.hotelName);
+              break;
+            }
+          }
+      })
      })
   })
-
-  
